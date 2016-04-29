b011190b65a146ca4b001e7f6487900c87583454
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-04-29 15:26:16 -0300

    add class - GET and POST Requests

---
+++ mindspace-youtube/07-get-post/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3,] });
+});
+
+/* igor
+  pass a parameter to GET routes /:param
+*/
+router.get('/test/:id', function(req, res, next) {
+  res.render('test', {output: req.params.id});
+});
+
+router.post('/test/submit', function(req, res, next) {
+  // igor - Retrieve data from the post method
+  var id = req.body.id;
+  res.redirect('/test/' + id);
+});
+
+module.exports = router;
