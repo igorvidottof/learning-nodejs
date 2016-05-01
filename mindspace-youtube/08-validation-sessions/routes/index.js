87a09cd05383bada90e278568e070e3ebf761e89
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-04-30 22:31:56 -0300

    add class Express-Validator and Express-Session

---
+++ mindspace-youtube/08-validation-sessions/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Form Validation', success: req.session.success, errors: req.session.errors});
+  // igor - clear the errors after show them to the user
+  req.session.errors = null;
+  // igor - reset the session message after show it to the user
+  req.session.success = null;
+});
+
+router.post('/submit', function(req, res, next) {
+  // Check validity of the values
+  req.check('email', 'Invalid e-mail address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if(errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } 
+  else {
+    req.session.success = true;
+  }
+  res.redirect('/');
+});
+
+module.exports = router;
