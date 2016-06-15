6859be4badd3f13bf387d2d8e43fd411f08c37ce
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-06-15 08:36:47 -0300

    add lecture code

---
+++ mindspace-youtube/shopping-cart/08-validation-user-signup/config/passport.js
@@ -0,0 +1,52 @@
+var passport = require('passport');
+var User = require('../models/user');
+var LocalStrategy = require('passport-local').Strategy;
+
+// tell how to store the user in the session
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// creating a new strategy
+passport.use('local.signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  // validate user credentials
+  req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+  req.checkBody('password', 'Invalid password').notEmpty().isLength({ min: 4 });
+  // get error messages
+  var errors = req.validationErrors();
+  if(errors) {
+    var messages = [];
+    errors.forEach(function(error) {
+      messages.push(error.msg);
+    });
+    return done(null, false, req.flash('error', messages));
+  }
+
+  User.findOne({'email': email}, function(err, user) {
+    if(err) {
+      return done(err);
+    }
+    if(user) {
+      return done(null, false, {message: 'Email is already in use'});
+    }
+    var newUser = new User();
+    newUser.email = email;
+    newUser.password = newUser.encryptPassword(password);
+    newUser.save(function(err, result) {
+      if(err) {
+        return done(err);
+      }
+      return done(null, newUser);
+    });
+  });
+}));
---
+++ mindspace-youtube/shopping-cart/08-validation-user-signup/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+// import csurf for csrf protection, avoiding that a user session gets stolen
+var csrf = require('csurf');
+var passport = require('passport');
+
+// import Product model
+var Product = require('../models/product');
+
+// start csrf protection
+var csrfProtection = csrf();
+router.use(csrfProtection); // all routers should be protected by csrfProtection
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // fetch all products(docs) and send them to the index.hbs file
+  Product.find(function(err, docs) {
+    // each row must contain only 3 thumbnails, therefore 3 products
+    var productChunks = [];
+    var chunkSize = 3;
+    // create multiple arrays containing 3 docs(products) each
+    for(var i = 0; i < docs.length; i += chunkSize) {
+      productChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  });
+});
+
+// user routes
+router.get('/user/signup', function(req, res, next) {
+  // extract error messages from the flash package
+  var messages = req.flash('error');
+  res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
+});
+
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true // display the flash msg created in passport.js
+}));
+
+router.get('/user/profile', function(req, res, next) {
+  res.render('user/profile');
+});
+
+module.exports = router;
