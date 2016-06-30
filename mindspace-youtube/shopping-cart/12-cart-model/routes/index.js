d0ac77a5e8b636ab9106bd5a6894a129e60c9241
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-06-29 22:39:04 -0300

    add lecture code

---
+++ mindspace-youtube/shopping-cart/12-cart-model/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var Cart = require('../models/cart');
+
+var router = express.Router();
+
+// import Product model
+var Product = require('../models/product');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // fetch all products(docs) and send them to the index.hbs file
+  Product.find(function(err, docs) {
+    // each row must contain only 3 thumbnails, therefore 3 products
+    var productChunks = [];
+    var chunkSize = 3;
+    // create multiple arrays containing 3 docs(products) each
+    for(var i = 0; i < docs.length; i += chunkSize) {
+      productChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  // if a cart is already living in a session, receives this cart
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if(err) {
+      // added by myself
+      return res.render('views/error', { message: err });
+      // original code: return res.redirect('/');
+    }
+    cart.add(product, productId);
+    // assign the new cart to a session variable
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
