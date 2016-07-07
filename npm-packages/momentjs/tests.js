edd884f13bdb49fa5f98738e3f5e8603c3cc0542
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-07-07 10:14:55 -0300

    add moment.js tests

---
+++ npm-packages/momentjs/tests.js
@@ -0,0 +1,16 @@
+const moment = require('moment');
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+console.log(moment().format());
+console.log(moment().startOf('day').fromNow());
+
+var now = moment();
+var tenDaysAhead = now.clone().add(10, 'days');
+var tenDaysAgo = now.clone().subtract(10, 'days');
+
+console.log(now.format('L'));
+console.log(tenDaysAhead.format('L'));
+console.log(tenDaysAgo.format('L'));
+
+console.log(tenDaysAhead.isSameOrAfter(now));
+console.log(tenDaysAgo.isSameOrBefore(now));
