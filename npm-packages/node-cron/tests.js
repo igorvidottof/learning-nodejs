5a885f7fda6bc9ba94cd8a8e33f8b811c68ed3cd
Igor Vidotto Felipe <igor_vidotto@hotmail.com>
2016-07-09 22:37:25 -0300

    update test

--- npm-packages/node-cron/tests.js
+++ npm-packages/node-cron/tests.js
@@ -5,12 +5,18 @@ new CronJob('* * * * * *', function() {
   console.log('You will see this message every second');
 }, null, true);*/
 
-var job = new CronJob(new Date(), function() {
+var test = false;
+
+var jobDate = new Date('2016, 7, 9');
+
+var job = new CronJob(jobDate, function() {
   /* runs once at the specified date. */
-  console.log('It works');
+    test = true;
   }, function () {
     /* This function is executed when the job stops */
-    console.log('It worked');
+    if(test) {
+      console.log('It worked');
+    }
   },
   true /* Start the job right now */
   // timeZone /* Time zone of this job. */
